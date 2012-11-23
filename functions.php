53f4e1dd797c88ca5b5020736562814562f6c98e
Jacob Dubail <jacobdubail@gmail.com>
2012-11-23 13:07:50 -0800

    Update functions.php
    adding open graph

--- functions.php
+++ functions.php
@@ -108,3 +108,36 @@ function browser_body_class($classes) {
 	if($is_iphone) $classes[] = 'iphone';
 	return $classes;
 }
+
+
+function jtd_googleplus_contactmethod( $contactmethods ) {
+    if ( ! isset( $contactmethods['googleplus'] ) )
+        $contactmethods['googleplus'] = 'Google+';
+    return $contactmethods;
+}
+add_filter( 'user_contactmethods', 'jtd_googleplus_contactmethod' );
+add_action( 'wp_head', 'jtd_googleplus_header' );
+function jtd_googleplus_header() {
+    if ( is_singular() ) {
+        $gplus_link = get_the_author_meta( 'googleplus', get_current_user_id() );
+        if ( $gplus_link )
+            echo '<link rel="author" href="' . $gplus_link . '" />';
+    }
+}
+
+function jtd_opengraph_for_posts() {
+    if ( is_singular() ) {
+        global $post;
+        setup_postdata( $post );
+        $output = '<meta property="og:type" content="article" />' . "\n";
+        $output .= '<meta property="og:title" content="' . esc_attr( get_the_title() ) . '" />' . "\n";
+        $output .= '<meta property="og:url" content="' . get_permalink() . '" />' . "\n";
+        $output .= '<meta property="og:description" content="' . esc_attr( get_the_excerpt() ) . '" />' . "\n";
+        if ( has_post_thumbnail() ) {
+            $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+            $output .= '<meta property="og:image" content="' . $imgsrc[0] . '" />' . "\n";
+        }
+        echo $output;
+    }
+}
+add_action( 'wp_head', 'jtd_opengraph_for_posts' );
